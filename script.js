24275888a2104b4004eeb46ff6334f5b878c16cf
Aleksey <lesha.sinitsin@mail.ru>
2021-03-06 19:09:28 +0300

    add color for win

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const area = document.querySelector('.area');
 const sector = document.querySelectorAll('.sector');
 
-function win (sector, player){
+function win (sector, player, click){
     const box = document.querySelector('.win__block');
     const victory = [
         [0,1,2],
@@ -17,35 +17,36 @@ function win (sector, player){
     victory.forEach(item =>{
         if(sector[item[0]].innerHTML == sector[item[1]].innerHTML && sector[item[1]].innerHTML == sector[item[2]].innerHTML && sector[item[0]].innerHTML != ""){
             box.innerHTML = player;
+            sector[item[0]].style.color = "red";
+            sector[item[1]].style.color = "red";
+            sector[item[2]].style.color = "red";
             document.querySelector('.win').style.color = "red";
-            setInterval(() => {
-                window.location.reload();
-            }, 4000);
+            sector.forEach(item =>{
+                item.removeEventListener("click", click);
+            });
         }
-    })
-}
-function startGame (){
-    sector.forEach(item =>{
-        let counter = 0;
-        let player = "&#215;";
-        area.addEventListener('click', ()=>{
-            counter = counter + 1;
-        });
-        function click(e){
-            const stepNow = document.querySelector('.step__now');
-            stepNow.innerHTML = player;
-            if(counter%2 === 1){
-                player = "&#215;"
-                stepNow.innerHTML = "&#9675;"; 
-            }else{
-                player = "&#9675;"
-                stepNow.innerHTML = "&#215;";
-            }
-                item.innerHTML = player;
-            win(sector, player);
-            e.target.removeEventListener("click", click);
-        }
-        item.addEventListener('click', click)
     });
 }
-startGame();
+sector.forEach(item =>{
+    let counter = 0;
+    let player = "&#215;";
+    area.addEventListener('click', ()=>{
+        counter = counter + 1;
+    });
+    function click(e){
+        const stepNow = document.querySelector('.step__now');
+        stepNow.innerHTML = player;
+        if(counter%2 === 1){
+            player = "&#215;"
+            stepNow.innerHTML = "&#9675;"; 
+        }else{
+            player = "&#9675;"
+            stepNow.innerHTML = "&#215;";
+        }
+            item.innerHTML = player;
+        win(sector, player);
+        e.target.removeEventListener("click", click);
+    }
+    item.addEventListener('click', click);
+});
+
