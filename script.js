1416393e8f2c1232a6b251f8edef32b3c797c143
Aleksey <lesha.sinitsin@mail.ru>
2021-03-07 18:56:41 +0300

    bug fix

--- script.js
+++ script.js
@@ -50,20 +50,23 @@ function win (sector, player, x, area, stepNow, step, box){
     });
 }
 area.addEventListener('click', x = (e)=>{
-    all = all + 1;
+    
     if(e.target.innerHTML == 'x' || e.target.innerHTML == "o"){
         return;
     }else{
         e.target.addEventListener('click', handleClick(player, counter, e.target, stepNow, step), [true]);
         counter = counter + 1;
+        all = all + 1;
     }
+    win(sector, player, x, area, stepNow, step, box);
     sector.forEach(unit =>{
         console.log(all);
-        if((unit.innerHTML == 'x' || unit.innerHTML == 'o') && all>=9){
+        if((unit.innerHTML == 'x' || unit.innerHTML == 'o') && all >= 9){
             doBlue(unit);
             step.innerHTML = "";
             stepNow.innerHTML = "";
             box.innerHTML = `Ничья!`;
+            win(sector, player, x, area, stepNow, step, box);
         }
     });
 });
